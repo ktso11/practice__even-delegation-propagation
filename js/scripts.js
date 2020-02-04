1f2fc51131a19c606fc232ee6d3af78b72439e01
Katie So <katieso86@gmail.com>
2020-02-03 17:12:59 -0800

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+console.log("hello world");
+
+$(document).ready(function(){
+
+  document.querySelector('.grandparent');
+  var parent = document.querySelector('.btn-test');
+  var child = document.querySelector('#child');
+  var container = document.getElementsByClassName('container')
+  var createBtn = document.querySelector('.create-btn')
+  var btnDiv = document.querySelector('.btn-test')
+
+// Event Delegation Example
+  createBtn.addEventListener('click', insertBtn, false)
+
+  function insertBtn(){
+    var newDiv = document.createElement('Button')
+    newDiv.innerHTML = "I am a new button"
+    newDiv.setAttribute('class', 'btn')
+    btnDiv.appendChild(newDiv)
+  }
+  //Even Delegation
+  parent.addEventListener('click', function(event) {
+    console.log(event.target)
+    if(event.target.className==='btn'){
+      alert('You clicked me!');
+    }
+  });
+
+
+
+
+
+
+// Testing Event Capturing and Bubbling
+
+  for(i=0; i<container.length; i++){
+    //by default, true as the last parameter means capturing phase first
+    container[i].addEventListener('click', clickedEvent, true)
+    //false, will revert to bubbling phase -notify after the targetting phase
+    // container[i].addEventListener('click', clickedEvent, false)
+    // container[i].addEventListener('click', showAlert)
+  }
+
+function clickedEvent(e){
+    var phase = e.eventPhase;
+    //to stop bubbling up to parent node
+    // e.stopPropagation();
+    if(1=== phase){
+      phase = 'capturing'
+    } else if ( 2===phase){
+      phase = 'at target'
+    } else if (3 == phase){
+      phase = 'bubbling'
+    }
+    console.log('container: '+this.id+ ' - ' + phase)
+  };
+
+});
---
+++ readme.md
@@ -0,0 +1,9 @@
+A little game I created to explore Jquery on my own time. Inspired by turn based RPG, which relies heavily on text, and minimal UI.
+
+This is for comic relief. Please don't take it too seriously.
+
+- https://ktso11.github.io/life-RPG
+- Click on "Find Enemy" start
+- User can choose to attack or use magic on the enemy
+- Enemy will return attack in 2 seconds
+- Game is over when the user or the enemy is defeated.
